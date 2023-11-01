7c62869902c62471f07eed95d7f55c6d6c96e769
Tamojit_Dutta@epam.com <Tamojit_Dutta@epam.com>
2023-11-01 07:37:19 +0530

    added three space

--- src/main/java/com/test/Test/TestController.java
+++ src/main/java/com/test/Test/TestController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
+
 public class TestController {
 
     @GetMapping
     public ResponseEntity<TestBean> getTest() {
         TestBean testBean = TestBean.builder().code(300).message("Its ok").build();
         return ResponseEntity.ok(testBean);
+
     }
 
     @PostMapping
+
     public ResponseEntity<TestBean> postTest(@RequestBody TestBean testBean) {
         return new ResponseEntity<>(testBean, HttpStatus.CREATED);
     }
